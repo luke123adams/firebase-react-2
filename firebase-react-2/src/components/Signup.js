30148398f05aadd60fe7bd6eb74f83e62834e832
luke123adams <lxa207@gmail.com>
2023-08-24 11:50:52 +0100

    added error state and error message for signup module

--- firebase-react-2/src/components/Signup.js
+++ firebase-react-2/src/components/Signup.js
@@ -1,5 +1,5 @@
-import { React, useRef } from "react";
-import { Form, Button, Card } from 'react-bootstrap'
+import React, {  useRef, useState } from "react";
+import { Form, Button, Card, Alert } from 'react-bootstrap'
 import { useAuth } from '../contexts/AuthContext'
 
 export default function Signup() {
@@ -7,11 +7,24 @@ export default function Signup() {
     const passwordRef = useRef()
     const passwordConfirmRef = useRef()
     const { signup } = useAuth
+    const [error, setError] = useState('')
+    const [loading, setLoading] = useState(false)
 
-    function handleSubmit(e) {
+    async function handleSubmit(e) {
         e.preventDefault()
 
-        signup(emailRef.current.value, passwordRef.current.value)
+        if (passwordRef.current.value !==
+            passwordConfirmRef.current.value) {
+                return setError('Passwords do not match')
+            }
+            
+            try {
+                setError('')
+                setLoading(true)
+                await signup(emailRef.current.value, passwordRef.current.value)
+            } catch {
+                setError('Failed to create an account')
+            }
     }
     
     return (
@@ -19,7 +32,8 @@ export default function Signup() {
         <Card>
         <Card.Body>
         <h2 className="text-center mb-4">Sign up</h2>
-        <Form>
+        {error && <Alert variant="danger">{error}</Alert>}
+        <Form onSubmit={handleSubmit}>
         <Form.Group id="email">
             <Form.Label>Email</Form.Label>
             <Form.Control type="email" ref={emailRef} required></Form.Control>
@@ -32,7 +46,7 @@ export default function Signup() {
             <Form.Label>Confirm Password</Form.Label>
             <Form.Control type="password" ref={passwordConfirmRef} required></Form.Control>
         </Form.Group>
-        <Button className="w-100" type="submit">Sign Up</Button>
+        <Button disabled={loading} className="w-100" type="submit">Sign Up</Button>
         </Form>
         </Card.Body>
         </Card>
